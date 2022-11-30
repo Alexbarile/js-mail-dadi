f8bebc4b11550acd2c931663bd0432eb07122147
Alexbarile <alexbarile93fg@yahoo.it>
2022-11-30 17:44:43 +0100

    creato generatore di numeri con math.random

---
+++ Dadi/js/script.js
@@ -0,0 +1,7 @@
+// generare un numero casuale per il giocatore tra 1 e 6;
+
+let giocatore = Math.floor(Math.random() * 6) + 1;
+
+// generare un numero casuale per il computer tra 1 e 6;
+
+let computer = Math.floor(Math.random() * 6) + 1;

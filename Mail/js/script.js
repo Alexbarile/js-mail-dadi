7352daba0f6cb684083e28df3d960b36ff85cce0
Alexbarile <alexbarile93fg@yahoo.it>
2022-12-01 19:43:09 +0100

    bonus email

--- Mail/js/script.js
+++ Mail/js/script.js
@@ -1,30 +1,60 @@
-// creare l'ARRAY
+// // creare l'ARRAY
+
+// const lista = ['fabrizio@gmail.it', 'marco@gmail.it', 'alessio@gmail.it', 'alessandro@gmail.it'];
+
+// // chiedere all'utente l'email
+
+// let mail = prompt("Inserisci l'email"); 
+
+// // fare controllo se risulta la sua email
+
+// let flag = false;
+
+// for(let i = 0; i<lista.length; i++){
+    
+//     if(lista[i] == mail){
+//         flag = true;
+//     }
+// }
+
+// // SE: risulta allora uscirà un messaggio con scritto 'Hai effettuato l'accesso'
+
+// if(flag){
+//     console.log("Hai effettuato l'accesso");
+// }
+
+// // ALTRIMENTI: non risulta allora uscirà un messaggio con scritto 'Accesso negato'
+
+// else{
+//     console.log("Accesso negato");
+// }
+
+
+// BONUS
 
 const lista = ['fabrizio@gmail.it', 'marco@gmail.it', 'alessio@gmail.it', 'alessandro@gmail.it'];
 
-// chiedere all'utente l'email
+const check_email = document.getElementById('invio');
 
-let mail = prompt("Inserisci l'email"); 
+check_email.addEventListener('click', function(){
+    let user_mail = document.getElementById('email').value;
 
-// fare controllo se risulta la sua email
+    let flag = false;
 
-let flag = false;
+    for(let i = 0; i < lista.length; i++){
+        if(user_mail == lista[i]){
+            flag = true;
+        }
 
-for(let i = 0; i<lista.length; i++){
-
-    if(lista[i] == mail){
-        flag = true;
     }
-}
+    const tag_result = document.getElementById('accesso');
 
-// SE: risulta allora uscirà un messaggio con scritto 'Hai effettuato l'accesso'
+    if(flag){
+        tag_result.innerHTML = `Hai effettuato l'accesso`;
+             
+    }
+    else{
+        tag_result.innerHTML = `Accesso negato`;
+    }
 
-if(flag){
-    console.log("Hai effettuato l'accesso");
-}
-
-// ALTRIMENTI: non risulta allora uscirà un messaggio con scritto 'Accesso negato'
-
-else{
-    console.log("Accesso negato");
-}
+});
--- Mail/readme.txt
+++ Mail/readme.txt
@@ -7,3 +7,17 @@ PROBLEMA: Mail
       allora uscirà un messaggio con scritto 'Hai effettuato l'accesso'
     ALTRIMENTI: non risulta
       allora uscirà un messaggio con scritto 'Accesso negato'
+
+BONUS
+
+1 - realizzare campo input
+2 - realizzare pulsante
+3 - intercettare click sul pulsante 
+    3.1 - recuperare il tag del pulsante
+4 - all'interno del click sul pulsante inserire il controllo per verificare se l'email risulta o meno
+    4.1 - recuperare valore inserito nell'email
+    4.2 - controllo se l'email risulta o no
+5 - aggiungo un tag nell'html di avviso
+6 - iniettare il messaggio nel tag 
+    6.1 - recupero il tag tramite javascript
+    6.2 - iniettare il risutato del tag
